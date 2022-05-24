56be378ec55c73a81e0a222e879edfe38ec63863
inging12 <inging12@naver.com>
2022-05-24 13:58:09 +0900

    220524 appConfig

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,24 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    /**
+     * HashMap은 동시성 이슈가 발생할 수 있다.
+     * 이런 경우에는 ConcurrentHashMap을 사용하자.
+     */
+    private static Map<Long,Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}

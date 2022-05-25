5d00fc7b9929c172e147360050a9aaf89d61b901
inging12 <inging12@naver.com>
2022-05-25 16:44:09 +0900

    220525 singleton

--- src/test/java/hello/core/AppConfig.java
+++ src/test/java/hello/core/AppConfig.java
@@ -12,16 +12,23 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+//@Configuration 없이 @Bean만 사용해도 스프링 빈으로 등록되지만 싱글톤을 보장하지 않는다.
+//스프링 설정 정보는 항상 @Configuration을 사용할 것.
 @Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        //1번
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService(){
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
@@ -30,6 +37,8 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository(){
+        // 2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,46 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        //모두 같은 인스턴스를 참고하고 있다.
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        //모두 같은 인스턴스를 참고하고 있다.
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: class hello.core.AppConfig$$EnhancerBySpringCGLIB$$ad8aea39
+
+        // -> @Bean이 붙은 메서드마다 이미 스프링 빈이 존대하면 존대하는 빈을 반환하고
+        //    스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다.
+        //    덕분에 싱글톤이 보장되는 것!
+    }
+}
